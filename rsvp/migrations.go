069831d8e89274297015c680964add74d7a36f2e
biplobmanna <biplobmanna@outlook.com>
2025-06-22 18:28:35 +0530

    update: better printing of migrations

--- rsvp/migrations.go
+++ rsvp/migrations.go
@@ -6,14 +6,19 @@ import (
 	"gorm.io/gorm"
 )
 
-func Migrate[M Model](db *gorm.DB, m *M) {
-	fmt.Println("Migrating ", m)
+func Migrate[M Model](db *gorm.DB, m *M, desc ...string) {
+	printDesc := "database table..."
+	if len(desc) > 0 {
+		printDesc = desc[0]
+	}
+	fmt.Println("  🟣 Migrating ", printDesc)
 	db.AutoMigrate(&m)
 }
 
 func MigrateAll(db *gorm.DB) {
-	Migrate(db, &Admin{})
-	Migrate(db, &User{})
+	fmt.Println("🟣 Running All Migrations...")
+	Migrate(db, &Admin{}, "Admin")
+	Migrate(db, &User{}, "User")
 }
 
 func MigrateRefreshAndConnectDB() *gorm.DB {
