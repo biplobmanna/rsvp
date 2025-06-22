30f5de9430df6604b973505fc27463f4cfbb69ac
biplobmanna <biplobmanna@outlook.com>
2025-06-22 18:29:12 +0530

    update: better printing of seeders + data update

--- rsvp/seeders.go
+++ rsvp/seeders.go
@@ -3,7 +3,6 @@ package rsvp
 import (
 	"fmt"
 	"log"
-	"strconv"
 
 	"github.com/jaswdr/faker/v2"
 	"gorm.io/gorm"
@@ -15,12 +14,9 @@ var SEEDERS = 100
 func SeedAdmin(s Settings, db *gorm.DB) {
 	fake := faker.New()
 	errors, rowsAffected := 0, 0
-	for i := range 10 {
+	for range 2 {
 		user := fake.Internet().User()
-		token, err := EncryptAES(s, user+s.ADMIN_TOKEN+strconv.Itoa(i))
-		if err != nil {
-			log.Fatal(err)
-		}
+		token := s.ADMIN_TOKEN
 		admin := Admin{
 			User:  user,
 			Token: token,
@@ -33,10 +29,9 @@ func SeedAdmin(s Settings, db *gorm.DB) {
 			rowsAffected += 1
 		}
 	}
-	fmt.Println("Seeding Admin:")
-	fmt.Println("Rows Affected: ", rowsAffected)
-	fmt.Println("Errors While Seeding: ", errors)
-	fmt.Println()
+	fmt.Println("🔵 Seeding Admin:")
+	fmt.Println("  🔵 Rows Affected: ", rowsAffected)
+	fmt.Println("  🔵 Errors While Seeding: ", errors)
 }
 
 func seedUser(s Settings, db *gorm.DB) {
@@ -44,17 +39,19 @@ func seedUser(s Settings, db *gorm.DB) {
 	errors, rowsAffected := 0, 0
 	for i := range SEEDERS {
 		username := fake.Internet().User()
-		token, err := EncryptAES(s, username+s.ADMIN_TOKEN+strconv.Itoa(i))
+		token, err := EncryptAES(s, username+s.ADMIN_TOKEN)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		user := User{
 			User:     username,
-			Token:    token,
 			FullName: fake.Person().Name(),
+			Token:    token,
+			Email:    fake.Internet().Email(),
+			Phone:    "+91-9123456789",
 			Rsvp:     false,
-			Comments: "",
+			Comments: fake.Lorem().Sentence(i),
 		}
 		result := db.Create(&user)
 
@@ -64,8 +61,7 @@ func seedUser(s Settings, db *gorm.DB) {
 			rowsAffected += 1
 		}
 	}
-	fmt.Println("Seeding User:")
-	fmt.Println("Rows Affected: ", rowsAffected)
-	fmt.Println("Errors While Seeding: ", errors)
-	fmt.Println()
+	fmt.Println("🔵 Seeding User:")
+	fmt.Println("  🔵 Rows Affected: ", rowsAffected)
+	fmt.Println("  🔵 Errors While Seeding: ", errors)
 }
