99099d04be3a92d51cca1a1fe8ade2f7a371e2d5
biplobmanna <biplobmanna@outlook.com>
2025-06-22 18:29:29 +0530

    update: adding more URLs for users CRUD

--- rsvp/urls.go
+++ rsvp/urls.go
@@ -2,14 +2,32 @@ package rsvp
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
 )
 
-func AddUrls(app *fiber.App, db *gorm.DB) {
+func AddUrls(app *fiber.App) {
 	// Add the URLs
 	app.Get("/", IndexView).Name("index")
 	app.Get("/whoami", WhoAmIView).Name("whoami")
 	app.Get("/card", CardView).Name("card")
 
 	app.Post("/check-whoami", CheckWhoAmI).Name("check-whoami")
+
+	// Admin View
+	admin := app.Group("/admin").Name("admin-")
+	admin.Get("/users", AdminViewUsers).Name("all-users-get")
+	admin.Post("/users", AdminViewUserCrud).Name("users-add")
+	admin.Get("/users/:id", AdminViewUserCrud).Name("users-get")
+	admin.Post("/users/:id", AdminViewUserCrud).Name("users-patch")
+	admin.Delete("/users/:id", AdminViewUserCrud).Name("users-delete")
+	admin.Get("/whoami", AdminCheckWhoAmI).Name("whoami-get")
+	admin.Post("/whoami", AdminCheckWhoAmI).Name("whoami-post")
+
+	// all other admin urls will be redirected to /admin/whoami
+	admin.All("/*", RedirectToAdmin).Name("redirect")
+	// all other urls will be redirected to /whoami
+	app.All("/*", RedirectToIndex).Name("index-redirect")
+
+	// Print All URLS
+	// stack, _ := json.MarshalIndent(app.Stack(), "", " ")
+	// fmt.Println(string(stack))
 }
