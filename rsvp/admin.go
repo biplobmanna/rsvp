e9e7e03a4b2cceb3dc519ecc3b7f0d69336de709
biplobmanna <biplobmanna@outlook.com>
2025-06-30 22:17:15 +0530

    update: reorg
    - reorg the URLs with more comments
    - reorg views with more comments
    - separate admin views -> admin.go
    - more comments everywhere

---
+++ rsvp/admin.go
@@ -0,0 +1,98 @@
+package rsvp
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RedirectToAdmin(c *fiber.Ctx) error {
+	return c.Redirect("/admin/whoami")
+}
+
+func AdminCheckWhoAmI(c *fiber.Ctx) error {
+	if c.Method() == "GET" {
+		return c.Render("whoami", fiber.Map{
+			"Title":          "RSVP: Admin",
+			"CheckWhoAmIUrl": "/admin/whoami",
+		}, "base")
+	} else if c.Method() == "POST" {
+		whoami, err := GetTokenCookie(c)
+		if err != nil {
+			return c.SendStatus(fiber.StatusExpectationFailed)
+		}
+		if whoami.ValidateToken() {
+			return c.SendString("Token Validated")
+		}
+
+	}
+	return c.SendStatus(fiber.StatusBadRequest)
+}
+
+func AdminViewUsers(c *fiber.Ctx) error {
+	var results []User
+	DB.Table("users").Find(&results)
+
+	return c.Render("users", fiber.Map{
+		"Title": "RSVP: Admin - Users",
+		"Users": results,
+	}, "base")
+}
+
+func AdminViewUserCrud(c *fiber.Ctx) error {
+	if c.Method() == "GET" {
+		param := c.Params("id")
+		var user User
+		var update bool
+		id, err := strconv.Atoi(param)
+
+		if param == "new" {
+			user = User{}
+			update = false
+		} else if err == nil {
+			// fetch data from DB
+			queryResult := DB.Table("users").First(&user, id)
+			if queryResult.Error != nil {
+				return c.SendStatus(fiber.StatusNotFound)
+			}
+			update = true
+		} else {
+			return c.SendStatus(fiber.StatusNotFound)
+		}
+
+		return c.Render("user", fiber.Map{
+			"Title":  "Rsvp: Admin - Edit User",
+			"Update": update,
+			"User":   user,
+		}, "base")
+	} else if c.Method() == "POST" {
+		user := User{}
+		if err := c.BodyParser(&user); err != nil {
+			c.SendStatus(fiber.StatusExpectationFailed)
+		}
+		user.Comments = strings.TrimSpace(user.Comments)
+
+		// update the DB
+		result := DB.Save(&user)
+		if result.Error != nil {
+			c.SendStatus(fiber.StatusExpectationFailed)
+		}
+		fmt.Printf("✅ User {ID: %d} updated...\n", user.ID)
+		return c.Redirect("/admin/users")
+	} else if c.Method() == "DELETE" {
+		id := c.Params("id")
+		var user User
+		queryResult := DB.Delete(&user, id)
+		if queryResult.Error != nil {
+			return c.SendStatus(fiber.StatusNotFound)
+		}
+		return c.SendStatus(fiber.StatusNoContent)
+	}
+	return c.SendStatus(fiber.StatusBadRequest)
+}
+
+func AdminView(c *fiber.Ctx) error {
+	return c.SendString("Admin View")
+}
--- rsvp/urls.go
+++ rsvp/urls.go
@@ -5,29 +5,40 @@ import (
 )
 
 func AddUrls(app *fiber.App) {
-	// Add the URLs
-	app.Get("/", IndexView).Name("index")
+	// ****************************************************************
+	// URLs for non-admin users
+	// ****************************************************************
+	// Validate normal User
 	app.Get("/whoami", WhoAmIView).Name("whoami")
+	app.Post("/whoami", WhoAmIView).Name("whoami-check")
+
+	// Card view for the validated users
 	app.Get("/card", CardView).Name("card")
 
-	app.Post("/check-whoami", CheckWhoAmI).Name("check-whoami")
-
-	// Admin View
+	// ****************************************************************
+	// URLs related to ADMIN
+	// ****************************************************************
+	// Group all Admin URLs
 	admin := app.Group("/admin").Name("admin-")
-	admin.Get("/users", AdminViewUsers).Name("all-users-get")
+	admin.Get("/users", AdminViewUsers).Name("users-get-all")
+
+	// Users CRUD Operations
 	admin.Post("/users", AdminViewUserCrud).Name("users-add")
 	admin.Get("/users/:id", AdminViewUserCrud).Name("users-get")
-	admin.Post("/users/:id", AdminViewUserCrud).Name("users-patch")
+	//? since default form only supports GET/POST, using instead of PATCH
+	admin.Post("/users/:id", AdminViewUserCrud).Name("users-post")
 	admin.Delete("/users/:id", AdminViewUserCrud).Name("users-delete")
-	admin.Get("/whoami", AdminCheckWhoAmI).Name("whoami-get")
-	admin.Post("/whoami", AdminCheckWhoAmI).Name("whoami-post")
+
+	// Validate Admin
+	admin.Get("/whoami", AdminCheckWhoAmI).Name("admin-whoami")
+	admin.Post("/whoami", AdminCheckWhoAmI).Name("admin-whoami-check")
 
 	// all other admin urls will be redirected to /admin/whoami
-	admin.All("/*", RedirectToAdmin).Name("redirect")
-	// all other urls will be redirected to /whoami
-	app.All("/*", RedirectToIndex).Name("index-redirect")
+	admin.All("/*", RedirectToAdmin).Name("admin-redirect")
 
-	// Print All URLS
-	// stack, _ := json.MarshalIndent(app.Stack(), "", " ")
-	// fmt.Println(string(stack))
+	// ***************************************************************
+	// MISC URLs
+	// ***************************************************************
+	// all other urls will be redirected to /whoami
+	app.All("/*", RedirectToWhoAmI).Name("whoami-redirect")
 }
